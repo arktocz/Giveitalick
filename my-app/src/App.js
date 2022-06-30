3116cd5cdfd5a46378c6d4d1246d96c1bd0f2cc0
arktocz <arkto@gmail.com>
2022-06-30 09:33:21 +0200

    Lets go for documentation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import React, { useState, useRef } from 'react'
 //import './index.js'
 
+/** @module Zadost */
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
   );
 }
 
+
 function Col(props) {
   let className = "col";
   if (props.size) {
@@ -29,35 +31,21 @@ function Col(props) {
   )
 }
 
-function TextAreaAsk(props) {
-  // console.log(props.data.request_history[0].content);
 
-  // const [value, setvalue] = useState('')
+function TextAreaAsk(props) {
   const onchange=(e)=>{
-    // console.log(e.target.value)
-    // setvalue(e.target.value)
     props.OnTextAskChange(e.target.value)
   }
-  // let last_element=props.data.request_history.lenght-1;
-  // console.log(typeof(props.data.request_history))
- 
   return(
     <div className="form-outline">
-      {/* <label class="form-label" for="textAreaExample">Message</label> */}
-      {/* <input type="textarea"></input> */}
-      
-      {/* <textarea className="form-control" id="TextAreaAsk" rows="20" defaultValue={props.data.request_history.slice(-1)[0].content} onChange={onchange}   ></textarea> */}
       <textarea className="form-control" id="TextAreaAsk" rows="20" defaultValue={props.data.request_history[props.data.request_history.length-1].content} onChange={onchange}   ></textarea>
-      
     </div>
   )
   
 }
 
 
-
 function TableUserData(props){
-  
   let request_history = props.request_history.map(
     (item, index) => (
       <Row key={item.id}>
@@ -67,11 +55,9 @@ function TableUserData(props){
         <Col>{item.state}</Col>
         <Col>{item.editor_id}</Col>
         <Col>{item.editor_name}</Col>
-      
       </Row>
     )
   )
-
   return(
     <Row>
       {request_history}
@@ -82,10 +68,8 @@ function TableUserData(props){
 
 
 function Cardcomponent(props) {
-  // console.log(props.data.request_history[0]);
   const [request_history, setRequest_history] = React.useState(props.data.request_history)  
   return(
-    
     <Card>
       <CardHeader>
         <Row>
@@ -104,41 +88,28 @@ function Cardcomponent(props) {
   )
 }
 
+
 function TextAreaRespose(props){
-  // const [value, setvalue] = useState('')
   const onchange=(e)=>{
-    // console.log(e.target.value)
-    // setvalue(e.target.value)
     props.OnTextResponseChange(e.target.value)
   }
   return(
     <div class="form-outline margin-top:10">
-      {/* <label class="form-label" for="textAreaExample">Message</label> */}
       <textarea class="form-control" id="TextAreaRespose" rows="18" placeholder="Odpověd na žádost" onChange={onchange} ></textarea>
-      
     </div>
   )
-  
 }
 
+
 function Buttons(props) {
-  
-  // const pressedbutton="";
   const [value, setvalue] = useState('')
   const ButtonPressed=(e)=>{
-  
     setvalue(e.target.value)
-    // props.OnSetClick(value)
   }
-
   const saveButtonHandler = () => {
-
     props.returnNewRecord(value);
   }
-
-  
   return(
-    
     <div class="btn-group"  role="group" aria-label="Basic radio toggle button group">
       <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" value="Schvaleno" onClick={ButtonPressed} ></input>
       <label class="btn btn-outline-primary" htmlFor="btnradio1">Schváleno</label>
@@ -150,14 +121,9 @@ function Buttons(props) {
       <label class="btn btn-outline-primary" htmlFor="btnradio3">Zamítnuto</label>
       &nbsp;
       <button className='btn btn-primary' onClick={saveButtonHandler}>ULOŽIT</button>
-    </div>
-   
-    
+    </div>   
   )
-  
 }
-
-
 
 
 function Row(props) {
@@ -173,6 +139,7 @@ function CardHeader(props) {
       )
 }
 
+
 function CardBody(props) {
   return (
       <div className="card-body" > { props.children } </div>
@@ -180,13 +147,9 @@ function CardBody(props) {
 }
 
 
-
 function Textcomponent(props) {
-
-
   const [textAreaResponse, setTextAreaResponse] = useState("");
   const [textAreaAsk, setTextAreaAsk] = useState("");
-
 
   function OnTextAskChange(value){
     setTextAreaAsk(value);
@@ -205,50 +168,48 @@ function Textcomponent(props) {
     let year = newDate.getFullYear();
     let fulldate= `${date}.${month}.${year}`;
 
-    const newRecord={'id':0, 'date':fulldate, 'poznamka':textAreaResponse, 'editor_id':0, 'editor_name':'Odpovídač', 'state':status, 'content':textAreaAsk};
+    let fakeID=Math.random() * (500 - 100) + 100;
+    fakeID=Math.round(fakeID);
+   
+    const nameDict={405:"Spytihněv",521:"Božidara",480:"Haštal",222:"Servác",253:"Kařesomysl",785:"Irmhild",542:"Voršila",632:"Kordula",564:"Zvonimíra"}
+    let keys = Object.keys(nameDict)
+    let values = Object.values(nameDict)
+    let numberOfElement=Math.floor(Math.random() * keys.length)
+    let fakeEditorID = keys[numberOfElement]
+    let fakeName=values[numberOfElement]
+    
+    const newRecord={'id':fakeID, 'date':fulldate, 'poznamka':textAreaResponse, 'editor_id':fakeEditorID, 'editor_name':fakeName, 'state':status, 'content':textAreaAsk};
     console.log(newRecord);
     props.onEditReqHistory(newRecord);
 
   }
-
-
- 
   return(
       <Card>
         <CardHeader>Vaše žádost</CardHeader>
         <CardBody>
+
             <Row>
-                  <Col size="col-md-8">
-                      <TextAreaAsk{...props} OnTextAskChange={OnTextAskChange}/>
-                      
-                  </Col>
-                  <Col size="col-md-3">
-                    <Row ><Buttons{...props} returnNewRecord={returnNewRecord}/></Row>
-
-                    <Row><TextAreaRespose{...props} OnTextResponseChange={OnTextResponseChange}/></Row>
-                    
-                  </Col>
-
+              <Col size="col-md-8">
+                <TextAreaAsk{...props} OnTextAskChange={OnTextAskChange}/>                     
+              </Col>
+              <Col size="col-md-3">
+                <Row ><Buttons{...props} returnNewRecord={returnNewRecord}/></Row>
+                <Row><TextAreaRespose{...props} OnTextResponseChange={OnTextResponseChange}/></Row>
+              </Col>
             </Row>
-           
+
         </CardBody>
       </Card>
-  )
-  
+  ) 
 }
+
+
 function Tablecomponent(props) {
   return(
-    //<div>
-      //<Row>Historie úprav:</Row>
-      //{/* zkusit předelat do card */}
-      //<Row><Cardcomponent{...props} /></Row>
-
-     //</div>
   <Card>
     <CardHeader>Historie úprav</CardHeader>
     <CardBody>
         <Row> <Cardcomponent{...props} /> </Row>
-       
     </CardBody>
   </Card>
   )
@@ -262,11 +223,13 @@ function CardFooter(props) {
       )
 }
 
+
 function Card(props) {
   return (
       <div className="card" > { props.children } </div>
       )
 }
+
 
 function PageMedium(props) {
   return (
@@ -291,54 +254,35 @@ function ExtraInfo(props) {
 
 function PageLarge(props) {
 
-const [data, setdata]=useState(props.data);
-let data2=props.data;
-// console.log("data2 jsou:",data2);
-// console.log(data)
+  const [data, setdata]=useState(props.data);
+  let data2=props.data;
   return (
-      <Card>
-          {/* <CardHeader>Vaše žádost</CardHeader> */}
-          <CardBody>
-          <Row><Textcomponent{...props} onchange={setdata}/></Row>
-          <Row><Tablecomponent{...props}/></Row>
-          
-          </CardBody>
-          <button className='btn btn-primary' onClick={()=>setdata(data2)}>set</button>
-      </Card>
+    <Card>
+        <CardBody>
+        <Row><Textcomponent{...props} onchange={setdata}/></Row>
+        <Row><Tablecomponent{...props}/></Row>
+        </CardBody>
+    </Card>
   )
 }
 
 
 function PageStoryBook(props) {
-  
-  // const reqHistoryDATA= [
-  //   {'id':89, 'date':'1.2.2020', 'poznamka':'props1', 'editor_id':622, 'editor_name':'Alexandr', 'state':'nevyřízeno', 'content':"obsah žadosti"},
-  //   {'id':90, 'date':'1.2.2020', 'poznamka':'props1', 'editor_id':622, 'editor_name':'Alexandr', 'state':'nevyřízeno', 'content':"obsah žadosti2"},
-  //   // {'id':89, 'date':'1.2.2020', 'poznamka':'props2', 'editor_id':622, 'editor_name':'Alexandr', 'state':'nevyřízeno','content':{'rows':[]}},
-  //   // {'id':89, 'date':'1.2.2020', 'poznamka':'props3', 'editor_id':622, 'editor_name':'Alexandr','state':'nevyřízeno', 'content':{'rows':[]}},
-  // ];
-
   const extraProps = {
-    
     'data':{
     'name':2,
     'request_history':[
       {'id':89, 'date':'1.2.2020', 'poznamka':'props1', 'editor_id':622, 'editor_name':'Alexandr', 'state':'nevyřízeno', 'content':"obsah žadosti"},
       {'id':89, 'date':'1.2.2020', 'poznamka':'props1', 'editor_id':622, 'editor_name':'Alexandr', 'state':'nevyřízeno', 'content':"obsah žadosti"},
       {'id':89, 'date':'1.2.2020', 'poznamka':'konec', 'editor_id':622, 'editor_name':'Alexandr', 'state':'nevyřízeno', 'content':"obsah žadostilast"},
-      
-      // {'id':89, 'date':'1.2.2020', 'poznamka':'props3', 'editor_id':622, 'editor_name':'Alexandr','state':'nevyřízeno', 'content':{'rows':[]}},
-    
     ],
   }};
 
   const [reqHistory, setReqHistory ]= useState(extraProps);
-  
-  
+
   const onEditReqHistory= (newHistoryRecord) =>{
     console.log("edit history called")
     console.log(newHistoryRecord)
-   
     setReqHistory((prevHistory) => {
       let prevHistory2=prevHistory.data.request_history;
       console.log(prevHistory.data.request_history)
@@ -346,28 +290,11 @@ function PageStoryBook(props) {
       return {...prevHistory, request_history: `${prevHistory2}`};
     });
   }
-  
-
-
-
-
-  // 'data':{
-  // 'id':15,
-  // 'content':{'rows':[]},
-  // 'editor': {'id':622, 'name': 'Alexander'},
-  // 'history':[
-  //   {'id':89, 'date':'1.2.2020', 'poznamka':'bla1', 'editor':{'id':622, 'name':'Alexandr'}, 'content':{'rows':[]}},
-  //   {'id':90, 'date':'1.2.2020', 'poznamka':'bla2', 'editor':{'id':666, 'name':'Josef'}, 'content':{'rows':[]}},
-  
-  // ]
-  // }};
   return <PageLarge {...extraProps} {...props} onEditReqHistory={onEditReqHistory}/>
 }
 
 function Page(props) {
   return <PageStoryBook {...props} />
 }
-
-
 
 export default App;
